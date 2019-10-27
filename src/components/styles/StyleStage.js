abad3ebf853a8b2e31e610cdc8cf503beda10dfd
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-10-27 07:35:27 -0600

    StyleStage created and added to Stage component

--- src/components/styles/StyleStage.js
+++ src/components/styles/StyleStage.js
@@ -5,9 +5,12 @@ export const StyledStage = styled.div`
     /* is going to repeat what we're graving from props */ 
     grid-template-rows: repeat(
         ${props => props.height},
+        /* css calculation to make the grid look good 
+        and keep the aspect ratio of the grid cells*/
         calc(25vw / ${props => props.width})
     );
-    grid-template-columns: repeat(${props => props.width}, 1fn);
+    /*this calculation is for keep perfect squares */
+    grid-template-columns: repeat(${props => props.width}, 1fr);
     grid-gap: 1px;
     border: 2px, solid #333;
     width: 100%
